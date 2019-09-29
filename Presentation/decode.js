b7fead1635097536aa7d53656533e3255a917347
cassienoelle <cassienoelle@gmail.com>
2019-09-28 21:01:24 -0400

    tidied up decode

--- Presentation/decode.js
+++ Presentation/decode.js
@@ -6,18 +6,20 @@ Javascript snippet to create a 'decoder'
 animation on selected text
 
 Cassie Smith - Fall 2019
+inspired by code by Lauri: https://codepen.io/bionik/pen/dzBweB
 
 */
 
 let $randomString = "";
-let $symbols = '$%#@*!378'
+let symbols;
+let symbolsNoSpaces;
 let $initString;
-let $outputString;
-let $currentChar;
+let outputString;
+let displayedString;
 let $currentSlide;
 let $decodeElement;
 let clicked = false;
-let decodedIndexes = [];
+let characters = [];
 
 $(document).ready(function() {
     console.log("ready!");
@@ -29,8 +31,8 @@ $(document).ready(function() {
 
       if (!clicked) {
         setStrings();
-        generateRandomString($outputString);
-        decodeText();
+        generateRandomString(outputString);
+        setDecoder();
         clicked = true;
       }
       else {
@@ -44,11 +46,27 @@ $(document).ready(function() {
 //
 // set the initial string as the current leetspeak title
 // set the output string as the english version of that title
+// set symbols as string of combined characters of first two
+// make a version of symbols without spaces so first and last character are never 'empty'
 function setStrings() {
   $initString = $decodeElement.text();
-  $outputString = "What is it?";
+  outputString = "What is it?";
+  symbols = $initString + outputString;
+
+  for (let i = 0; i < symbols.length; i++){
+    if (symbols[i] == " "){
+      symbolsNoSpaces = symbols.substr(0,i) + symbols.substr(i + 1);
+    }
+  }
+
   console.log('init = ' + $initString);
-  console.log('output = ' + $outputString);
+  console.log('output = ' + outputString);
+}
+
+
+function setCharAt(str,index,chr) {
+    // if(index > str.length-1) return str;
+    return str.substr(0,index) + str.substr(index+1);
 }
 
 // generateRandomString()
@@ -60,67 +78,110 @@ function generateRandomString(str) {
   console.log('chars needed: ' + numChars);
 
   for (let i = 0; i < str.length; i++) {
-    let r = Math.floor(Math.random() * $symbols.length);
-    $randomString += $symbols.charAt(r);
+    let r = Math.floor(Math.random() * symbols.length);
+    $randomString += symbols.charAt(r);
     console.log($randomString);
   }
+}
+
+// setDecoder()
+//
+//
+function setDecoder() {
+  characters = [];
+
+  for (let i = 0; i < outputString.length; i++) {
+    characters.push({
+      countdown: Math.floor(Math.random() * 10),
+      final: outputString.charAt(i)
+    });
+  }
+
+  console.log(characters);
+  decodeText();
 }
 
 // decodeText()
 //
 //
 function decodeText() {
-  decodedIndexes = [];
-  let possibleIndexes = [];
+  let decoding = false;
+  let displayedString = "";
 
-  for (let i = 0; i < $outputString.length; i++) {
-    possibleIndexes.push(i);
-  }
+  for (let i = 0; i < characters.length; i++) {
+    let currentChar = characters[i];
 
-  console.log('okay! possible indexes is: ' + possibleIndexes);
-  let timing = setInterval(function() {
-    console.log('init length = ' + $initString.length);
-    console.log('out length = ' + $outputString.length);
+    if (currentChar.countdown > 0) {
+      currentChar.countdown--;
+      decoding = true;
 
-
-      let r = possibleIndexes[Math.floor(Math.random() * possibleIndexes.length)];
-      console.log('r = ' + r);
-      console.log('includes? ' + decodedIndexes.includes(r));
-
-
-      if (decodedIndexes.includes(r) == false) {
-        $initString = setCharAt($initString,r,$randomString.charAt(r));
-        decodedIndexes.push(r);
-        $(".decode").text($initString);
-      }
-      else {
-        $initString = setCharAt($initString,r,$outputString.charAt(r));
-        $(".decode").text($initString);
-        possibleIndexes.splice(findIndex(possibleIndexes,r), 1);
-        console.log('possible = ' + possibleIndexes);
-        // console.log('includes');
-        // console.log(decodedIndexes);
-        // console.log('init length: ' + $initString.length + 'decodedIndex length: ' + decodedIndexes.length);
+      if (i === 0 || i === characters.length - 1) {
+        displayedString += symbolsNoSpaces[Math.floor(Math.random() * symbolsNoSpaces.length)];
+      } else {
+        displayedString += symbols[Math.floor(Math.random() * symbols.length)];
       }
 
-      if ($initString == $outputString) {
-        console.log('init = ' + $initString);
-        console.log('output = ' + $outputString);
-        clearInterval(timing);
-        console.log('cleared');
-      }
-  }, 500);
-}
-
-function setCharAt(str,index,chr) {
-    // if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
-}
-
-function findIndex(array, value) {
-  for(let i = 0; i < array.length; i++){
-    if (array[i] === value) {
-      return i;
+    }
+    else {
+      displayedString += currentChar.final;
     }
   }
+
+  $decodeElement.text(displayedString);
+
+  if (decoding) {
+    setTimeout(decodeText, 25);
+  }
+  else {
+    console.log('done!');
+  }
+
 }
+
+
+//
+// // decodeTextArchived()
+// //
+// //
+// function decodeTextArchived() {
+//   decodedIndexes = [];
+//   let possibleIndexes = [];
+//
+//   for (let i = 0; i < outputString.length; i++) {
+//     possibleIndexes.push(i);
+//   }
+//
+//   console.log('okay! possible indexes is: ' + possibleIndexes);
+//   let timing = setInterval(function() {
+//     console.log('init length = ' + $initString.length);
+//     console.log('out length = ' + outputString.length);
+//
+//
+//       let r = possibleIndexes[Math.floor(Math.random() * possibleIndexes.length)];
+//       console.log('r = ' + r);
+//       console.log('includes? ' + decodedIndexes.includes(r));
+//
+//
+//       if (decodedIndexes.includes(r) == false) {
+//         $initString = setCharAt($initString,r,$randomString.charAt(r));
+//         decodedIndexes.push(r);
+//         $(".decode").text($initString);
+//       }
+//       else {
+//         $initString = setCharAt($initString,r,outputString.charAt(r));
+//         $(".decode").text($initString);
+//         possibleIndexes.splice(findIndex(possibleIndexes,r), 1);
+//         console.log('possible = ' + possibleIndexes);
+//         // console.log('includes');
+//         // console.log(decodedIndexes);
+//         // console.log('init length: ' + $initString.length + 'decodedIndex length: ' + decodedIndexes.length);
+//       }
+//
+//       if ($initString == outputString) {
+//         console.log('init = ' + $initString);
+//         console.log('output = ' + outputString);
+//         clearInterval(timing);
+//         console.log('cleared');
+//       }
+//   }, 50);
+// }
