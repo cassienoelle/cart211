d60df50baba7fbbbbc9cbd99beffc455b85c43b6
cassienoelle <cassienoelle@gmail.com>
2019-09-30 12:33:01 -0400

    added styling and javascript to animate and display result

--- Exercises/Lab_Sept30/script.js
+++ Exercises/Lab_Sept30/script.js
@@ -1,7 +1,29 @@
+"use strict";
+
+let input;
+let result;
+let container;
+let greeting;
+
+document.addEventListener("DOMContentLoaded", ()=> {
+  console.log('ready');
+  container = document.getElementById("hello");
+  input = document.getElementById("inputName");
+  result = document.getElementById("result");
+
+  container.style.width = (input.offsetWidth * 2) + "px";
+  result.style.width = input.offsetWidth;
+  result.classList.add("hide");
+
+  console.log(input.offsetWidth);
+  console.log(result.offsetWidth);
+  console.log(result.classList);
+})
 
 function sayHello() {
-  let message1 = document.getElementById("name").value;
-  let message2 = document.getElementById("name2").value;
-  let message3 = message1 + " " + message2;
-  document.getElementById("helloTo").innerHTML = message3;
+  let message1 = document.getElementById("firstName").value;
+  let message2 = document.getElementById("lastName").value;
+  result.classList.remove("hide");
+  result.style.width = input.offsetWidth + "px";
+  document.getElementById("helloTo").innerHTML = message1 + "<br/>" + message2;
 }
